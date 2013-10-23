f15e27b0f8b836571f5cef3369159e29d641d6e1
yehchge <yehchge@gmail.com>
2013-10-23 23:37:58 +0800

    首頁頭尾加入

--- controllers/index.php
+++ controllers/index.php
@@ -10,7 +10,10 @@ class Index extends Controller {
 		//echo Hash:create('sha256', 'jesse', HASH_PASWORD_KEY);
 		//echo Hash:create('sha256', 'test2', HASH_PASWORD_KEY);
 		$this->view->title = 'Home';
+
+		$this->view->render('header');
 		$this->view->render('index/index');
+		$this->view->render('footer');
 	}
 
 }
