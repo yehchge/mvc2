63190042120963c0d4c6fedc714aa80c6362a682
bill.yeh <bill.yeh@iwant-in.net>
2018-06-17 15:52:36 +0800

    Update SQL schema

--- sql/mvc.sql
+++ sql/mvc.sql
@@ -20,7 +20,7 @@ CREATE TABLE IF NOT EXISTS `data` (
   `dataid` int(11) NOT NULL AUTO_INCREMENT,
   `text` varchar(255) NOT NULL,
   PRIMARY KEY (`dataid`)
-) ENGINE=MyISAM AUTO_INCREMENT=59 DEFAULT CHARSET=utf8mb4;
+) ENGINE=MyISAM AUTO_INCREMENT=62 DEFAULT CHARSET=utf8mb4;
 
 -- 正在導出表  mvc2.data 的資料：10 rows
 /*!40000 ALTER TABLE `data` DISABLE KEYS */;
@@ -46,12 +46,13 @@ CREATE TABLE IF NOT EXISTS `note` (
   `content` text NOT NULL,
   `date_added` datetime NOT NULL,
   PRIMARY KEY (`noteid`)
-) ENGINE=MyISAM AUTO_INCREMENT=11 DEFAULT CHARSET=utf8mb4;
+) ENGINE=MyISAM AUTO_INCREMENT=14 DEFAULT CHARSET=utf8mb4;
 
--- 正在導出表  mvc2.note 的資料：3 rows
+-- 正在導出表  mvc2.note 的資料：4 rows
 /*!40000 ALTER TABLE `note` DISABLE KEYS */;
 INSERT INTO `note` (`noteid`, `title`, `userid`, `content`, `date_added`) VALUES
 	(1, 'Jesse', 8, 'This is a note.', '0000-00-00 00:00:00'),
+	(13, 'Jesse2', 34, 'Jesse2 message.', '2018-06-17 00:13:01'),
 	(8, 'DOG FACE', 34, '111', '2013-10-17 19:31:08'),
 	(10, 'Merry', 8, '這是一個測試', '2018-06-16 16:57:00');
 /*!40000 ALTER TABLE `note` ENABLE KEYS */;
@@ -81,17 +82,18 @@ CREATE TABLE IF NOT EXISTS `user` (
   `login` varchar(25) NOT NULL,
   `password` varchar(32) NOT NULL,
   `role` enum('default','admin','owner') NOT NULL DEFAULT 'default',
+  `login_session` varchar(32) NOT NULL DEFAULT '',
   PRIMARY KEY (`userid`)
 ) ENGINE=MyISAM AUTO_INCREMENT=37 DEFAULT CHARSET=utf8mb4;
 
 -- 正在導出表  mvc2.user 的資料：5 rows
 /*!40000 ALTER TABLE `user` DISABLE KEYS */;
-INSERT INTO `user` (`userid`, `login`, `password`, `role`) VALUES
-	(1, 'bill', 'f2f5a8950c0e218967dda7120648288a', 'owner'),
-	(7, 'test12', '09c78f479e758519ce90d69ddc19c3e4', 'owner'),
-	(8, 'demo', '88f2dccb02b2a20615211e5492f85204', 'owner'),
-	(34, 'jesse2', '8aa8c12fd71338d4ed652ffe41cdb792', 'default'),
-	(35, 'test33', '37017ef30e2adb8e9de2846a03e6caa1', 'default');
+INSERT INTO `user` (`userid`, `login`, `password`, `role`, `login_session`) VALUES
+	(1, 'bill', 'f2f5a8950c0e218967dda7120648288a', 'owner', ''),
+	(7, 'test12', '09c78f479e758519ce90d69ddc19c3e4', 'owner', ''),
+	(8, 'demo', '88f2dccb02b2a20615211e5492f85204', 'owner', '34229989384a4cb26ca5c0b3bdba4ae8'),
+	(34, 'jesse2', '8aa8c12fd71338d4ed652ffe41cdb792', 'default', '603e08b1a82f160ae9012afb9be5c33c'),
+	(35, 'test33', '37017ef30e2adb8e9de2846a03e6caa1', 'default', '');
 /*!40000 ALTER TABLE `user` ENABLE KEYS */;
 /*!40101 SET SQL_MODE=IFNULL(@OLD_SQL_MODE, '') */;
 /*!40014 SET FOREIGN_KEY_CHECKS=IF(@OLD_FOREIGN_KEY_CHECKS IS NULL, 1, @OLD_FOREIGN_KEY_CHECKS) */;
