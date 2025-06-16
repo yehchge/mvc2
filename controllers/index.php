1fcda1c50fc2f516ad6f24e2b12062aef6d5481d
bill.yeh <bill.yeh@iwant-in.net>
2025-06-16 19:00:01 +0800

    update

--- controllers/index.php
+++ controllers/index.php
@@ -13,7 +13,7 @@ class index extends Controller
         //echo Hash:create('sha256', 'test2', HASH_PASWORD_KEY);
         $this->view->title = 'Home';
 
-        $this->view->render('header');
+        $this->view->render('header', ['title' => 'Create a news item']);
         $this->view->render('index/index');
         $this->view->render('footer');
     }
